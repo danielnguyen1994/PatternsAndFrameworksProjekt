76b0af3b01ae273c1e01eac05e2647e4efb8684e
danielgnuyen1994 <danielnguyen@gmx.net>
2021-11-22 23:58:55 +0100

    Variable verändert

--- PafProjekt/src/application/Main.java
+++ PafProjekt/src/application/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			int a ;
 		} catch(Exception e) {
 			//sadasdas
 			
